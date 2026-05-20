e0088f968ffcb853c02a4562d6df89e84957cb3f
Nurulloh <nurullohkomilov18@gmail.com>
2026-05-20 22:13:00 +0500

    a

--- js/main.js
+++ js/main.js
@@ -263,3 +263,25 @@ closeMenu();
     });
   }
 });
+
+const reveals=document.querySelectorAll(
+".reveal,.slide-left,.slide-right"
+);
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+entry.target.classList.add("visible");
+}
+
+});
+
+},{
+threshold:.15
+});
+
+reveals.forEach(el=>{
+observer.observe(el);
+});
